53984cfef619b17bcc0947f80d40fa9777af4b4f
owaisfarooq <owais.farooq.qureshi@gmail.com>
2023-07-17 06:02:06 +0500

    New font and more functionality
    -added continuous calculations
    
    -added new font
    
    https://www.fontspace.com/typo-digit-font-f93576
    
    https://www.pagecloud.com/blog/how-to-add-custom-fonts-to-any-website#:~:text=How%20to%20add%20custom%20fonts%20to%20your%20website%20using%20%40font%2Dface%C2%A0
    
    -Fixed small bugs

--- script.js
+++ script.js
@@ -1,4 +1,72 @@
-function addDigit (digit) {
+function isFunciton(digit) {
+    switch (digit) {
+        case '+':
+            return true;
+        case '-':
+            return true;
+        case '*':
+            return true;
+        case '/':
+            return true;
+    }
+    return false;
+}
+
+function addDigit(digit) {
+    const func = document.getElementById("funcd");
     const output = document.getElementById("output");
-    output.value += digit;        
+    const LHS = document.getElementById("LHSoutput");
+
+    if (isFunciton(digit)) {
+        if (output.value == "") {
+            func.value = digit;
+            return 0;
+        }
+        if (LHS.value) {
+            LHS.value += func.value + output.value;
+            LHS.value = eval(LHS.value);
+            output.value = "";
+            func.value = digit;
+            return 0;
+        }
+        LHS.value += func.value;
+        func.value = digit;
+        LHS.value += output.value;
+        output.value = "";
+        
+        return 0;
+    }
+
+    output.value += digit;
+}
+
+function evaluateSum() {
+    const output = document.getElementById("output");
+    const LHS = document.getElementById("LHSoutput");
+    const func = document.getElementById("funcd");
+
+    LHS.value += func.value + output.value;
+    func.value = "";
+    output.value = "";
+    LHS.value = eval(LHS.value + output.value);
+}
+
+function clearFunction () {
+    const output = document.getElementById("output");
+    const LHS = document.getElementById("LHSoutput");
+    const func = document.getElementById("funcd");
+
+    func.value = "";
+    LHS.value = "";
+    output.value = "";
+}
+
+function backspace () {
+    const func = document.getElementById("funcd");
+    const output = document.getElementById("output");
+    if (output.value) {
+        output.value = output.value.slice(0, -1);
+    } else {
+        func.value = "";
+    }
 }
